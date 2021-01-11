60221acf0c2423025945cd39af26ee0117e1524a
ANIKET JADHAV <76157881+aniyy117@users.noreply.github.com>
2021-01-11 23:41:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let mainNav = document.getElementById('menu');
+let navBarToggle = document.getElementById('toggle');
+
+navBarToggle.addEventListener('click', function () {
+    
+    mainNav.classList.toggle('active');
+});
+
